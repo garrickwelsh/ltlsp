8eef273447044aa5cd076f3c7a7f2de701f6429c
Garrick Welsh <garrick.welsh@gmail.com>
2024-04-05 23:17:42 +1100

    Add tests for markdown

--- src/tree_sitter/mod.rs
+++ src/tree_sitter/mod.rs
@@ -18,4 +18,50 @@ mod tests {
         assert_eq!(root_node.start_position().column, 0);
         assert_eq!(root_node.end_position().column, 12);
     }
+
+    #[test]
+    fn test_rust_identify_comments() {
+        let mut parser = Parser::new();
+        parser
+            .set_language(tree_sitter_rust::language())
+            .expect("Error loading Rust grammar");
+        let source_code = r###"""
+        /// This is a comment..
+        fn test() {}
+        """###;
+        let tree = parser.parse(source_code, None).unwrap();
+        let root_node = tree.root_node();
+
+        println!("{:?}", root_node);
+        println!("{:?}", root_node.to_sexp());
+        let mut cursor = root_node.walk();
+        for i in root_node.children(&mut cursor) {
+            println!("{:?}", i);
+        }
+        assert_eq!(root_node.kind(), "source_file");
+        assert_eq!(root_node.start_position().column, 0);
+        assert_eq!(root_node.end_position().column, 10);
+    }
+    #[test]
+    fn test_markdown_identify_text() {
+        let mut parser = Parser::new();
+        parser
+            .set_language(tree_sitter_md::language())
+            .expect("Error loading Rust grammar");
+        let source_code = r###"""# Heading
+        Some text with a [https://github.com/](Github)
+        """###;
+        let tree = parser.parse(source_code, None).unwrap();
+        let root_node = tree.root_node();
+
+        println!("{:?}", root_node);
+        println!("{:?}", root_node.to_sexp());
+        let mut cursor = root_node.walk();
+        for i in root_node.children(&mut cursor) {
+            println!("{:?}", i);
+        }
+        assert_eq!(root_node.kind(), "document");
+        assert_eq!(root_node.start_position().column, 0);
+        assert_eq!(root_node.end_position().column, 10);
+    }
 }
