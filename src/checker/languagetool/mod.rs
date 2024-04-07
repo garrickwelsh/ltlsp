479c48bb7aac4f479190c1d581b0b9644f35c8dc
Garrick Welsh <garrick.welsh@gmail.com>
2024-04-07 22:30:00 +1000

    Add tests that use Serde to make a request to LanguageTool.

--- src/checker/languagetool/mod.rs
+++ src/checker/languagetool/mod.rs
@@ -1,12 +1,15 @@
 use reqwest;
+use serde::{Deserialize, Serialize};
 
 #[cfg(test)]
 mod tests {
 
     use std::collections::HashMap;
 
+    use super::*;
     use tokio;
 
+    #[ignore]
     #[tokio::test]
     async fn query_language_tool() -> Result<(), Box<dyn std::error::Error>> {
         let client = reqwest::Client::new();
@@ -27,4 +30,72 @@ mod tests {
         println!("{:?}", res.text().await?);
         Ok(())
     }
+
+    #[tokio::test]
+    async fn query_language_tool_with_serde() -> Result<(), Box<dyn std::error::Error>> {
+        let client = reqwest::Client::new();
+
+        let mut request = HashMap::new();
+        request.insert("language", "en-AU");
+        println!("request created");
+
+        let mut data = LanguageToolDocumentData {
+            annotation: Vec::<LanguageToolText>::new(),
+        };
+        data.annotation.push(LanguageToolText::Markup("<h1>"));
+        data.annotation.push(LanguageToolText::Text(
+            "Here is som text that I'd like spell checked.",
+        ));
+        data.annotation.push(LanguageToolText::Text(
+            "Is this something you're able to help me with?",
+        ));
+        data.annotation.push(LanguageToolText::Markup("</h1>"));
+
+        let request_data = serde_json::to_string_pretty(&data).unwrap();
+        request.insert("data", &request_data);
+
+        println!("{}", &request_data);
+
+        println!("request populated with data");
+        let res = client
+            .post("http://localhost:8081/v2/check")
+            .form(&request)
+            .send()
+            .await;
+        println!("{:?}", res);
+        let res = res?;
+        println!("{:?}", res);
+        println!("{:?}", res.text().await?);
+        Ok(())
+    }
 }
+
+#[derive(Serialize, Debug)]
+pub(crate) struct LanguageToolDocumentData<'a> {
+    annotation: Vec<LanguageToolText<'a>>,
+}
+
+#[derive(Serialize, Debug)]
+pub(crate) enum LanguageToolText<'a> {
+    #[serde(rename = "text")]
+    Text(&'a str),
+    #[serde(rename = "markup")]
+    Markup(&'a str),
+}
+
+// #[derive(Serialize, Debug)]
+// pub(crate) struct LanguageToolRequest<'a> {
+//     language: &'a str,
+//     data: LanguageToolDocumentData<'a>,
+// }
+
+// impl LanguageToolRequest<'_> {
+//     fn new<'a>() -> LanguageToolRequest<'a> {
+//         LanguageToolRequest {
+//             language: "en-AU",
+//             data: LanguageToolDocumentData {
+//                 annotation: Vec::<LanguageToolText>::new(),
+//             },
+//         }
+//     }
+// }
--- src/checker/mod.rs
+++ src/checker/mod.rs
@@ -1,1 +1,3 @@
 mod languagetool;
+
+pub trait Checker {}
