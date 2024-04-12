ad751ad1e463da37dd6ba24ad7518b5d589416bc
Garrick Welsh <garrick.welsh@gmail.com>
2024-04-13 00:33:39 +1000

    Hook the lsp-server to main.cs and setup tokio as the async engine.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
+use std::error::Error;
+
 use tracing::info;
 
 mod checker;
+mod lsp_server;
 mod tree_sitter;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error + Sync + Send>> {
     tracing_subscriber::fmt::init();
     info!("Hello, world!");
+    Ok(())
 }
