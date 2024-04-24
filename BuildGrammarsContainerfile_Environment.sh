090e4b74962f430127da489303126bd5d3f45ea6
Garrick Welsh <garrick.welsh@gmail.com>
2024-04-24 22:56:07 +1000

    Add terminal environment variable.

--- BuildGrammarsContainerfile_Environment.sh
+++ BuildGrammarsContainerfile_Environment.sh
@@ -4,4 +4,5 @@ podman build -f BuildGrammarsContainerfile -t ltlsp-dev -v $(pwd):/workspace
 podman run -i \
     --userns=keep-id \
     --mount type=bind,source=$(pwd),destination=/workspace,chown=false \
+    --env "TERM=$TERM" \
      ltlsp-dev bash
