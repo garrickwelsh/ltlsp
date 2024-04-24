cc06af050038f1286d37942ec13f4e0c758ddb53
Garrick Welsh <garrick.welsh@gmail.com>
2024-04-25 00:02:29 +1000

    (feat) Add true colour terminal support for podman container

--- BuildGrammarsContainerfile_Environment.sh
+++ BuildGrammarsContainerfile_Environment.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+export TERM=xterm-kitty
+
 podman build -f BuildGrammarsContainerfile -t ltlsp-dev -v $(pwd):/workspace
 podman run -it \
     --userns=keep-id \
     --mount type=bind,source=$(pwd),destination=/workspace,chown=false \
-    --env "TERM=$TERM" \
+    --env "TERM=kitty" \
      ltlsp-dev bash
