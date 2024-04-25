cf2607cc13f744b35ba9c26131befda58f1dbad4
Garrick Welsh <garrick.welsh@gmail.com>
2024-04-25 21:43:37 +1000

    (feat) Build rust grammar

--- runtime/build_languages.sh
+++ runtime/build_languages.sh
@@ -3,13 +3,19 @@
 function build_tree_sitter() {
   FOLDER="$1"
   GRAMMAR="$2"
-  LIBRARY="$3"
+  REF="$3"
+  LIBRARY="$4"
+
+  # curl "$GRAMMAR" -o grammar.js
+  # tree-sitter generate
+
+  git clone --depth 1 https://github.com/tree-sitter/tree-sitter-rust.git
 
   mkdir -p "$FOLDER"
   pushd "$FOLDER"
 
-  curl "$GRAMMAR" -o grammar.js
-  tree-sitter generate
+  git checkout 473634230435c18033384bebaa6d6a17c2523281
+  
   make
   #cp "$LIBRARY" 
   popd
@@ -17,6 +23,7 @@ function build_tree_sitter() {
 
 pushd "$(dirname "$0")"
 
-build_tree_sitter "rust" "https://raw.githubusercontent.com/tree-sitter/tree-sitter-rust/master/grammar.js" "libtree-sitter-rust.so"
+build_tree_sitter "tree-sitter-rust" "https://github.com/tree-sitter/tree-sitter-rust.git" "473634230435c18033384bebaa6d6a17c2523281" "libtree-sitter-rust.so"
+# build_tree_sitter "rust" "https://raw.githubusercontent.com/tree-sitter/tree-sitter-rust/master/grammar.js" "libtree-sitter-rust.so"
 
 popd
