160ca7fee6b1e3bb5dbda202355b6c4c92dbaacc
Garrick Welsh <garrick.welsh@gmail.com>
2024-05-29 22:39:18 +1000

    (feat): Fix spelling/grammar checking so it works on updates.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // #![allow(clippy::print_stderr)]
 #![feature(async_fn_traits)]
 
+use anyhow::Context;
 use lsp_types::notification::PublishDiagnostics;
 use lsp_types::request::CodeActionRequest;
 use lsp_types::CodeAction;
@@ -18,7 +19,6 @@ use lsp_types::{InitializeParams, ServerCapabilities};
 use lsp_server::{Connection, ExtractError, Message, Request, RequestId, Response};
 
 use std::collections::HashMap;
-use std::rc::Rc;
 use std::{error::Error, fs::OpenOptions};
 
 use tracing::info;
@@ -46,7 +46,6 @@ async fn main_loop(
     let _params: InitializeParams = serde_json::from_value(params).unwrap();
     let _documents = HashMap::<String, Document>::new();
     let mut document_checker = DocumentLanguageToolChecker::new().await;
-    let mut version: i32 = 0;
     info!("starting example main loop");
     for msg in &connection.receiver {
         info!("got msg: {msg:?}");
@@ -92,80 +91,86 @@ async fn main_loop(
             Message::Notification(not) => {
                 info!("got notification: {not:?}");
                 if not.method == "textDocument/didOpen" || not.method == "textDocument/didChange" {
-                    let mut diagnostics = Vec::<Diagnostic>::new();
-                    // Proof of Concept: Send back an error.
                     if let serde_json::Value::Object(map) = not.params {
                         if let serde_json::Value::Object(document_map) = map["textDocument"].clone()
                         {
-                            let document_parsed = document_checker
-                                .parse_str(
-                                    "rust",
-                                    document_map["uri"].as_str().expect("Expected document uri"),
-                                    i32::try_from(
-                                        document_map["version"]
-                                            .as_i64()
-                                            .expect("Expected a version"),
-                                    )?,
+                            if not.method == "textDocument/didOpen" {
+                                process_document(
+                                    &connection,
+                                    &mut document_checker,
+                                    &document_map,
                                     document_map["text"]
                                         .as_str()
-                                        .expect("Expected document text"),
+                                        .context("Expected document uri")?,
                                 )
                                 .await?;
-                            info!("Document parsed {:?}", document_parsed);
-                            info!("Document text is: {}", document_map["text"]);
-                            info!("File uri is: {}", document_map["uri"]);
-                            let Some(document_parsed) = document_parsed else {
-                                continue;
-                            };
-                            for i in &document_parsed.diagnostics {
-                                diagnostics.push(i.into());
+                            } else if not.method == "textDocument/didChange" {
+                                if let serde_json::Value::Object(change_map) =
+                                    map["contentChanges"].clone()
+                                {
+                                    process_document(
+                                        &connection,
+                                        &mut document_checker,
+                                        &document_map,
+                                        change_map["text"]
+                                            .as_str()
+                                            .context("Expected document uri")?,
+                                    )
+                                    .await?;
+                                }
                             }
                         }
                     }
-
-                    // // Send a test notification diagnostic.
-                    // if version == 0 {
-                    //     let diagnostic = Diagnostic {
-                    //         range: Range::new(
-                    //             Position {
-                    //                 line: 1,
-                    //                 character: 1,
-                    //             },
-                    //             Position {
-                    //                 line: 1,
-                    //                 character: 5,
-                    //             },
-                    //         ),
-                    //         // downgrade to hint if we're pointing at the macro
-                    //         severity: Some(lsp_types::DiagnosticSeverity::HINT),
-                    //         code: Some(lsp_types::NumberOrString::String(
-                    //             "An error for as an example".to_string(),
-                    //         )),
-                    //         code_description: None,
-                    //         source: Some("Language tool".to_string()),
-                    //         message: "Fix yee spelling".to_string(),
-                    //         related_information: None,
-                    //         tags: None,
-                    //         data: None, // Some(serde_json::json!({ "rendered": rd.rendered })),
-                    //     };
-                    //     diagnostics.push(diagnostic);
-                    // }
-                    let diagnostic_params = PublishDiagnosticsParams::new(
-                        lsp_types::Url::parse("file:///home/gaz/devel/ltlsp/test.ltlsp")?,
-                        diagnostics,
-                        Some(version),
-                    );
-                    version += 1;
-                    let not = lsp_server::Notification::new(
-                        <lsp_types::notification::PublishDiagnostics as lsp_types::notification::Notification>::METHOD
-                            .to_owned(),
-                        diagnostic_params,
-                    );
-                    connection.sender.send(Message::Notification(not))?;
                 }
             }
         }
     }
+    Ok(())
+}
+
+async fn process_document(
+    // not: &Notification,
+    connection: &Connection,
+    document_checker: &mut DocumentLanguageToolChecker,
+    document_map: &serde_json::Map<String, serde_json::Value>,
+    document_text: &str,
+) -> anyhow::Result<()> {
+    let mut diagnostics = Vec::<Diagnostic>::new();
+    info!("Running document checker.");
+    let document_parsed = document_checker
+        .parse_str(
+            "rust",
+            document_map["uri"]
+                .as_str()
+                .context("Expected document uri")?,
+            i32::try_from(
+                document_map["version"]
+                    .as_i64()
+                    .context("Expected a version")?,
+            )?,
+            document_text,
+        )
+        .await?;
+    info!("Document parsed {:?}", document_parsed);
+    info!("Document text is: {}", document_map["text"]);
+    info!("File uri is: {}", document_map["uri"]);
+    let Some(document_parsed) = document_parsed else {
+        return Ok(());
+    };
+    for i in &document_parsed.diagnostics {
+        diagnostics.push(i.into());
+    }
+    let diagnostic_params = PublishDiagnosticsParams::new(
+        lsp_types::Url::parse(&document_parsed.document_uri)?,
+        diagnostics,
+        Some(document_parsed.document_version),
+    );
+    let not = lsp_server::Notification::new(
+        <lsp_types::notification::PublishDiagnostics as lsp_types::notification::Notification>::METHOD
+            .to_owned(),
+        diagnostic_params,
+        );
+    connection.sender.send(Message::Notification(not))?;
     Ok(())
 }
 
